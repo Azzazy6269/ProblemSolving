1824f94a9afb335469257416892e2016e13308d5
Azzazy6269 <150115419+Azzazy6269@users.noreply.github.com>
2025-09-02 23:48:05 +0300

    Upload first three problems

---
+++ 02 - Postfix Problem Using Stack/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main()
+    {
+      Console.WriteLine(  postfix("55/2+19*-"));// = -6
+
+    }
+
+    static int postfix(string Equation)
+    {
+        int x, y, z = 0;
+        var s = new Stack<int>();
+        foreach (var i in Equation)
+        {
+            if (i == '+' || i == '-' || i == '*' || i == '/')
+            {
+                x = Convert.ToInt32(s.Pop());
+                y = Convert.ToInt32(s.Pop());
+                z = 0;
+                switch (i)
+                {
+                    case '+': z = y + x; break;
+                    case '-': z = y - x; break;
+                    case '*': z = y * x; break;
+                    case '/': z = y / x; break;
+                }
+                
+                s.Push(z);
+            }
+            else
+            {
+                s.Push(Convert.ToInt32(i-'0'));
+            }
+
+
+        }
+        return s.Pop();
+
+    }
+}

0e1ee155a3a9e2b59306eba28eea91e03b05cd45
Azzazy6269 <mohamadelazzazy@gmail.com>
2025-12-12 23:19:27 +0200

    Upload 7 problems

---
+++ 001 - Two Queues to simulate stack/Program.cs
@@ -0,0 +1,60 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
+public class MyStack
+{
+    Queue<int> q;
+
+    public MyStack()
+    {
+        q = new Queue<int>();
+    }
+    public int Pop()
+    {
+        var q2 = new Queue<int>();
+        int count = q.Count;
+        for (int i = 0; i < count - 1; i++)
+        {
+            q2.Enqueue(q.Dequeue());
+        }
+        int temp = q.Dequeue();
+        q = q2;
+        return temp;
+    }
+    public void Push(int element)
+    {
+        q.Enqueue(element);
+    }
+
+    public int Top()
+    {
+        var q2 = new Queue<int>();
+        int count = q.Count;
+        for (int i = 0; i < count - 1; i++)
+        {
+
+            q2.Enqueue(q.Dequeue());
+        }
+        int temp = q.Dequeue();
+        q2.Enqueue(temp);
+        q = q2;
+        return temp;
+    }
+
+    public bool Empty() => q.Count == 0;
+
+}
+
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.Push(x);
+ * int param_2 = obj.Pop();
+ * int param_3 = obj.Top();
+ * bool param_4 = obj.Empty();
+ */
---
+++ 034 - Find special substring of length k/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        Console.WriteLine(s.HasSpecialSubstring("asaacaaacc", 3));
+        Console.WriteLine(s.HasSpecialSubstring("asaxacaaacxc",2));
+    }
+}
+
+public class Solution
+{
+    public bool HasSpecialSubstring(string s, int k)
+    {
+        char current;
+        int count;
+        for (int i = 0; i < s.Length; i++) 
+        {
+            current = s[i];
+            count = 1;
+            while (i<s.Length-1 && current == s[i + 1])
+            {
+                i++;
+                count++;
+            }
+            if (count == k) 
+                return true;
+        }
+        return false;
+    }
+}
